e5614dc4c05a01e812f7377618868590dcbb5625
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-04-17 22:09:29 -0400

    added DOCKER_LOGIN, DOCKER_PWD and DOCKER_REGISTRY variables

--- build-push.sh
+++ build-push.sh
@@ -3,6 +3,10 @@
 IMAGE_NAME=bitnami-tomcat9-jdk18
 IMAGE_VER=1.0
 
+DOCKER_LOGIN=
+DOCKER_PWD=
+DOCKER_REGISTRY=registry-1.docker.io 
+
 docker build -f Dockerfile -t ${DOCKER_LOGIN}/${IMAGE_NAME}:${IMAGE_VER} .
 docker login --username=${DOCKER_LOGIN} --password ${DOCKER_PWD} ${DOCKER_REGISTRY}
 docker push ${DOCKER_LOGIN}/${IMAGE_NAME}:${IMAGE_VER}
