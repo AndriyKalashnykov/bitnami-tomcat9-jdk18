19b972ec44f24d5564ac06057b40b36d042048a7
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-04-18 19:54:20 -0400

    added images cleanup before build

--- build-push.sh
+++ build-push.sh
@@ -7,6 +7,9 @@ DOCKER_LOGIN=
 DOCKER_PWD=
 DOCKER_REGISTRY=registry-1.docker.io 
 
+docker rmi -f $(docker images | grep '<none>' | awk '{print $3}')  2>/dev/null
+docker rmi -f ${DOCKER_LOGIN}/$IMAGE_NAME:$IMAGE_VERSION
+
 docker build -f Dockerfile -t ${DOCKER_LOGIN}/${IMAGE_NAME}:${IMAGE_VER} .
 docker login --username=${DOCKER_LOGIN} --password ${DOCKER_PWD} ${DOCKER_REGISTRY}
 docker push ${DOCKER_LOGIN}/${IMAGE_NAME}:${IMAGE_VER}
